b3d3244ee2b4591488bd3a6a74cf7bc9c2ea9074
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-01-23 16:13:53 +0530

    final code

--- index.js
+++ index.js
@@ -3,30 +3,36 @@ const app = express();
 
 app.use(express.json());
 const path = require("path");
-const Database = require("better-sqlite3");
 
-const dbPath = path.join(process.cwd(), "bookstore.db");
+const {open} = require('sqlite');
+const sqlite3 = require('sqlite3');
+
+const dbPath = path.join(__dirname, "bookstore.db");
 
 const port = process.env.PORT || 3000;
 
-let db;
+let db = null;
 
-try {
-    // Initialize the database
-    db = new Database(dbPath, { verbose: console.log });
-    console.log("Connected to the database.");
-} catch (e) {
-    console.error(`DB Error: ${e.message}`);
-    process.exit(1);
-}
+const initializeDBAndServer = async () => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database
+        });
+        app.listen(port, () => {
+            console.log(`Server is Running at http://localhost:${port}`);
+        });
+    } catch (e) {
+        console.log(`DB Error: ${e.message}`);
+        process.exit(1);
+    }
+};
 
-// Root route
+
+initializeDBAndServer();
+
 app.get("/", (req, res) => {
     res.send("Mohan is the real hero!");
-});
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
 });
 
 module.exports = app;
